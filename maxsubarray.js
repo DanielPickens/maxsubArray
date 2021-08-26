40e5f7714bd4111471f27570f1545d248079c365
Daniel Pickens <72703981+DanielPickens@users.noreply.github.com>
2021-08-26 15:42:22 -0700

    Create maxsubarray.js

---
+++ maxsubarray.js
@@ -0,0 +1,35 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+## array  = nums to find max_sum from nums sub array = {1, 2 3, 4}
+
+        if not nums:
+            return 0
+        
+        current_sum = 0
+        max_sum = -sys.maxsize
+        
+        for n in nums:
+            
+            current_sum += n
+            max_sum = max(current_sum, max_sum)
+            
+            if current_sum < 0:
+                current_sum = 0
+           
+        return max_sum
+      
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
